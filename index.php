1520ad1b23e04d145708f687254fae547390cb66
Camila Guzman <67128218+camila-guzman@users.noreply.github.com>
2022-02-16 14:08:40 -0500

    upload base

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<?php include './templates/header.php'; ?>
+
+<div class="content col-lg-8">
+  <div class="container">
+  <!--
+  <div class="section">
+  <p> hi! my name is camila guzman</p>
+    <div class="blob">
+
+    </div>
+  </div> -->
+
+    <?php include './templates/about.php' ?>
+
+    <?php include './templates/portfolio.php' ?>
+
+    <?php include './templates/contact.php' ?>
+    
+    </div>
+  </div>
+
+</div>
+</div>
+</div>
+</body>
+
+</html>
