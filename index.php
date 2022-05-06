2078ffa9c59928ad57469386bbee97c53f6d8e7b
camilamguzman11@gmail.com <67128218+camila-guzman@users.noreply.github.com>
2022-05-05 22:06:11 -0400

    new additons
    hover image and filter set up

--- index.php
+++ index.php
@@ -7,6 +7,33 @@
       <div id="portfolio">
         <h1>Portfolio</h1>
 
+        <!--<form class="project-filter--form">
+          <div class="project-filter--div">
+            <input type="button" id="full-dev" name="full-dev" value="Full Stack Development">
+          </div>
+
+          <div class="project-filter--div">
+            <input type="button" id="front-dev" name="front-dev" value="Front End Development">
+          </div>
+
+          <div class="project-filter--div">
+            <input type="button" id="ux-ui" name="ux-ui" value="UX/UI">
+          </div>
+
+          <div class="project-filter--div">
+            <input type="button" id="design" name="design" value="Design">
+          </div>
+
+          <div class="project-filter--div">
+            <input type="button" id="content" name="content" value="Content">
+          </div>
+
+          <div class="project-filter--div">
+            <input type="button" id="management" name="management" value="Project Management">
+          </div>
+
+        </form>-->
+
         <div class="row project">
           <div class="col-lg-12">
             <h3>Lynx Bus Tracker App — Capstone Revamp Project</h3>
@@ -16,9 +43,12 @@
             </div>
             <div class="row">
               <div class="col-lg-3">
-                <a href="./images/lynx-app-filters-screen.png" class="img-hover"><img class="portfolio-img img-fluid mb-4" src="./images/lynx-app-filters-screen.png" /></a>
+                <a href="./images/lynx-app-filters-screen.png" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/lynx-app-filters-screen.png" />
+                </a>
               </div>
-              <div class="col-lg-7 mb-4">
+              <div class="col-lg-7 mb-3">
                 <p class="date">August 2021-December 2021</p>
                 <p>This was a group project completed for my university capstone. I collaborated with designers and a developer to revamp the existing Orlando's Lynx Bus Tracker App. We conceptualized, planned, and built the new Lynx Bus App on time.</p>
                 <p>I worked as a developer with another teammate to build out the new app using a front and back end tech stack. I introducted PHP, SQL, and MySQL to store and dynamically display bus route information and filter through routes!</p> 
@@ -45,16 +75,27 @@
               <span class="badge b-full-stack"><i class="fas fa-code"></i> Full Stack Development</span>
               <span class="badge b-ux-ui"><i class="fas fa-object-group"></i> UX/UI </span>
               <span class="badge b-design"><i class="fas fa-palette"></i> Design </span>
-              <span class="badge b-content"><i class="fas fa-edit"></i> Content Strategy </span>
+              <span class="badge b-content"><i class="fas fa-edit"></i> Content</span>
               <span class="badge b-lead"><i class="fas fa-tasks"></i> Project Management </span>
             </div>
             <div class="row p-2">
               <div class="col-lg-3">
-                <a href="./images/Aruba-guide-prototype-v1-landing.PNG" class="img-hover"><img class="portfolio-img img-fluid mb-4" src="./images/Aruba-guide-prototype-v1-landing.PNG" /></a>
-                <a href="./images/Aruba-guide-prototype-v1-typography.PNG" class="img-hover"><img class="portfolio-img img-fluid mb-4" src="./images/Aruba-guide-prototype-v1-typography.PNG" /></a>
-                <a href="./images/Aruba-guide-prototype-v1-contact-module.PNG" class="img-hover"><img class="portfolio-img img-fluid mb-4" src="./images/Aruba-guide-prototype-v1-contact-module.PNG" /></a>
+                <a href="./images/Aruba-guide-prototype-v1-landing.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/Aruba-guide-prototype-v1-landing.PNG" />
+                </a>
+                
+                <a href="./images/Aruba-guide-prototype-v1-typography.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/Aruba-guide-prototype-v1-typography.PNG" />
+                </a>
+                
+                <a href="./images/Aruba-guide-prototype-v1-contact-module.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/Aruba-guide-prototype-v1-contact-module.PNG" />
+                </a>
                 </div>
-              <div class="col-lg-7 mb-4">
+              <div class="col-lg-7 mb-3">
                 <p class="date">May 2021-December 2021</p>
                 <p>Purpose:</p>
                 <ul>
@@ -62,8 +103,10 @@
                   <li>Strengthen Aruba's web branding after their redesign launch</li>
                   <li>Make sure sites and web apps created by external teams follow new brand rules</li>
                 </ul>
+
+                <a class="cta" data-toggle="collapse" href="#aruba-collapse" role="button" aria-expanded="false" aria-controls="aruba-collapse">Read More</a>
                 
-                <div class="collapse" id="aruba-collapse show">
+                <div class="collapse" id="aruba-collapse">
                   <p>I categorized content, organized the information architecture, and created a low-fidelity prototype on Adobe XD.</p>
 
                   <p> After UX/UI iterations and content writing, I built a Wordpress theme using HTML, CSS, PHP, and Bootstrap. I wrote up documentation for the developers to use after I left the team.</p>
@@ -96,9 +139,11 @@
             </div>
             <div class="row">
               <div class="col-lg-3">
-                <a href="./images/osi-events-cover.png" class="img-hover"><img class="portfolio-img img-fluid mb-4" src="./images/osi-events-cover.png" /></a>
+                <a href="./images/osi-events-cover.png" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/osi-events-cover.png" /></a>
               </div>
-              <div class="col-lg-7 mb-4">
+              <div class="col-lg-7 mb-3">
                 <p class="date">May 2021</p>
                 <p> I designed and built a new Events page for UCF Office of Student Involvement's website. The page originally had a plain table with all of the semester's events.</p>
                 <p>The redesign focused on just <i>this week's</i> events to increase event and page engagement. I introduced jumplinks so that users can quickly look for events on a certain day.</p>
@@ -131,9 +176,11 @@
 
             <div class="row">
               <div class="col-lg-3">
-              <a href="./images/butterscotch-bakery-display.jpg" class="img-hover"><img class="img-fluid mb-4" src="./images/butterscotch-bakery-display.jpg" /></a>
+              <a href="./images/butterscotch-bakery-display.jpg" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/butterscotch-bakery-display.jpg" /></a>
             </div>
-              <div class="col-lg-7 mb-4">
+              <div class="col-lg-7 mb-3">
                 <p class="date">December 2020</p>
                 <p> I created a website for a fake bakery inspired by my love for baked goods!</p>
                 <p>I used MySQL to organize a database with two tables: one for account information with encrypted passwords and a second table for menu items and their details.</p>
@@ -166,9 +213,11 @@
             </div>
             <div class="row">
               <div class="col-lg-3">
-                <a href="./images/academic-village-screen.png" class="img-hover"><img class="img-fluid mb-4" src="./images/academic-village-screen.png" /></a>
+                <a href="./images/academic-village-screen.png" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/academic-village-screen.png" /></a>
             </div>
-              <div class="col-lg-7 mb-4">
+              <div class="col-lg-7 mb-3">
                 <p class="date">July 2020</p>
                 <p>This is a web design concept made for UCF's Historical Buildings Tour. My design centered on making the university archives pages more user-friendly for researchers and casual visitors alike, while following UCF's web brand standards.</p>
 
@@ -195,18 +244,26 @@
         <div class="row project">
           <h3>Portfolio Archive</h3>
           <p>Let's look back at past portfolios to see the growth :')</p>
-          <div class="col-lg-3 mb-4">
-            <a href="./images/portfolio-february-2022.PNG" class="img-hover"><img class="img-fluid mb-3" src="./images/portfolio-february-2022.PNG" /></a>
+          <div class="col-lg-3">
+            <a href="./images/portfolio-february-2022.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/portfolio-february-2022.PNG" /></a>
             <p>February 2022</p>
               <a class="cta" href="http://cg-portfolio-2-22.herokuapp.com/">Visit Site</a>
           </div>
-          <div class="col-lg-3 mb-4">
-            <img class="img-fluid mb-3" src="./images/portfolio-november-2020.PNG" />
+          <div class="col-lg-3">
+          <a href="./images/portfolio-february-2022.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/portfolio-november-2020.PNG" />
+          </a>
             <p>November 2020</p>
               <a class="cta" href="http://cg-portfolio-11-20.herokuapp.com/">Visit Site</a>
           </div>
-          <div class="col-lg-3 mb-4">
-            <img class="img-fluid mb-3" src="./images/portfolio-august-2020.PNG" />
+          <div class="col-lg-3">
+          <a href="./images/portfolio-february-2022.PNG" class="img-hover--a">
+                  <p class="img-hover--p">open image</p>
+                  <img class="portfolio-img img-fluid mb-4 img---hover-img" src="./images/portfolio-august-2020.PNG" />
+          </a>
             <p>August 2020</p>
               <a class="cta" href="hhttps://camila-guzman.github.io/">Visit Site</a>
           </div>
